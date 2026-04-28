6f2d19d847586129450fda5839a9b833b72f5e58
Eltomyalfon <tommyalfonzzo@gmail.com>
2026-04-28 19:23:11 -0300

    feat:Agregar archivos de comunicacion UART ESP32

--- Firmware/esp32_gateway/src/main.c
+++ Firmware/esp32_gateway/src/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "uart_comm.h"
 
-void app_main() {
-    printf("Hello!");
+/**
+ * @brief Punto de entrada principal del programa.
+ */
+void app_main(void) {
+    Uart_Init();
+
+    printf("\n=== ESP32 UART TEST ===\n");
+    printf("Presionar 'p' en el monitor serial para enviar PING a STM32\n");
+
+    while (1) {
+        int input = getchar();
+
+        if (input == 'p') {
+            Uart_SendPing();
+        }
+
+        Uart_ReadResponse();
+
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
 }
